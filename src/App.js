9b92bc39d072495e2093d0182a357b91a4f8d164
Manoel Lobo <manoel.lobo@gmail.com>
2017-11-22 14:30:40 -0300

    Init Repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/**
+ * GoNative - Desafio 03
+ * Manoel Thiago Nogueira
+ */
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+const App = () => (
+  <View />
+);
