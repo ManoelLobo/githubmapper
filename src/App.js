af5c784ad78bb359c833c8328179ffac566b1484
Manoel Lobo <manoel.lobo@gmail.com>
2017-11-27 20:15:13 -0300

    Bugfix: marcador carrega a imagem do usuário imediatamente após adição

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'config/ReactotronConfig';
 
 import React from 'react';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/es/integration/react'
+import { PersistGate } from 'redux-persist/es/integration/react';
 import Map from 'pages/Map';
 
 import { store, persistor } from 'redux/store';
--- src/components/Marker/index.js
+++ src/components/Marker/index.js
@@ -5,34 +5,42 @@ import MapView from 'react-native-maps';
 
 import styles from './styles';
 
-const Marker = ({ user, coord }) => (
-  <MapView.Marker
-    title={user.name || user.login}
-    description={user.bio || 'Sem biografia'}
-    coordinate={{ ...coord }}
-  >
-    <View style={styles.marker}>
-      <Image
-        key={user.avatar_url}
-        style={styles.avatar}
-        source={{ uri: user.avatar_url }}
-      />
-    </View>
-  </MapView.Marker>
-);
+export default class Marker extends React.Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      login: PropTypes.string,
+      bio: PropTypes.string,
+      id: PropTypes.number,
+      avatar_url: PropTypes.string,
+    }).isRequired,
+    coord: PropTypes.shape({
+      latitude: PropTypes.number,
+      longitude: PropTypes.number,
+    }).isRequired,
+  };
 
-Marker.propTypes = {
-  user: PropTypes.shape({
-    name: PropTypes.string,
-    login: PropTypes.string,
-    bio: PropTypes.string,
-    id: PropTypes.number,
-    avatar_url: PropTypes.string,
-  }).isRequired,
-  coord: PropTypes.shape({
-    latitude: PropTypes.number,
-    longitude: PropTypes.number,
-  }).isRequired,
-};
+  state = {
+    renderedImage: false,
+  }
 
-export default Marker;
+  render() {
+    const { user, coord } = this.props;
+    return (
+      <MapView.Marker
+        title={user.name || user.login}
+        description={user.bio || 'Sem biografia'}
+        coordinate={{ ...coord }}
+      >
+        <View style={styles.marker}>
+          <Image
+            key={`${user.avatar_url}.${this.state.renderedImage}`}
+            onLoad={() => { this.setState({ renderedImage: true }); }}
+            style={styles.avatar}
+            source={{ uri: user.avatar_url }}
+          />
+        </View>
+      </MapView.Marker>
+    );
+  }
+}
